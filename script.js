ed5d1725d7823e991e53d658a4d7c5ce121fb31c
kumachan96 <79151900+kumachan96@users.noreply.github.com>
2021-08-19 03:05:55 +0900

    Worked on control button, screen routing ✓

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
 // console.log(`Hello World 😊 `);
 
 const playButton = document.getElementById("play-btn");
+const pauseButton = document.getElementById("pause-btn");
+const mainMenuButton = document.getElementById("main-menu-btn");
+const playAgainButton = document.getElementById("play-again-btn");
 const mainScreen = document.getElementById("main-screen");
 const gameScreen = document.getElementById("game-screen");
 const gameOverScreen = document.getElementById("game-over-screen");
 
-playButton.addEventListener("click",startGame,false);
+playButton.addEventListener("click", startGame);
+pauseButton.addEventListener("click", pauseGame);
+playAgainButton.addEventListener("click", playAgain);
+mainMenuButton.addEventListener("click", mainMenu);
+
+function show_hide(showPage, hidePage) {
+  hidePage.classList.add("hide");
+  showPage.classList.remove("hide");
+}
 
 function startGame() {
-    console.log(`Game started`);
-    mainScreen.classList.add("hide");
-    gameScreen.classList.remove("hide");
+  console.log(`Game started`);
+  show_hide(gameScreen, mainScreen);
+}
+
+function pauseGame() {
+  console.log(`Game paused`);
+  show_hide(gameOverScreen, gameScreen);
+}
+
+function mainMenu() {
+  console.log(`Returning to main menu`);
+  show_hide(mainScreen, gameOverScreen);
+}
+
+function playAgain() {
+  console.log(`Pressed play again`);
+  show_hide(gameOverScreen, gameScreen);
+  // reset score and the timer
 }
