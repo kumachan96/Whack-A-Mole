48abcaf44808bfaa691e24945e02ba7b620f643b
kumachan96 <79151900+kumachan96@users.noreply.github.com>
2021-08-19 02:57:51 +0900

    Did some work on screens routing

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log(`Hello World 😊 `);
+// console.log(`Hello World 😊 `);
+
+const playButton = document.getElementById("play-btn");
+const mainScreen = document.getElementById("main-screen");
+const gameScreen = document.getElementById("game-screen");
+const gameOverScreen = document.getElementById("game-over-screen");
+
+playButton.addEventListener("click",startGame,false);
+
+function startGame() {
+    console.log(`Game started`);
+    mainScreen.classList.add("hide");
+    gameScreen.classList.remove("hide");
+}
