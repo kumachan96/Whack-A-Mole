9b36a3e197196a6c7ba1840e3a73ce0f94a728f0
kumachan96 <79151900+kumachan96@users.noreply.github.com>
2021-08-21 03:35:12 +0900

    Completed the Logic aspect of the game `script.js`

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // console.log(`Hello World 😊 `);
 
 const playButton = document.getElementById("play-btn");
-const pauseButton = document.getElementById("pause-btn");
+// const pauseButton = document.getElementById("pause-btn");
 const mainMenuButton = document.getElementById("main-menu-btn");
 const playAgainButton = document.getElementById("play-again-btn");
 const mainScreen = document.getElementById("main-screen");
@@ -11,7 +11,7 @@ const gameOverScreen = document.getElementById("game-over-screen");
 const GAME_TIME_SECONDS = 10;
 
 playButton.addEventListener("click", startGame);
-pauseButton.addEventListener("click", pauseGame);
+// pauseButton.addEventListener("click", pauseGame);
 playAgainButton.addEventListener("click", playAgain);
 mainMenuButton.addEventListener("click", mainMenu);
 
@@ -47,18 +47,20 @@ function startGame() {
   console.log(`Game started`);
   show_hide(gameScreen, mainScreen);
   timerReset();
+  startWhack();
   timerCountdown();
 }
 
 function gameOver() {
+  document.getElementById("game-over-score").innerHTML = score;
   show_hide(gameOverScreen, gameScreen);
 }
 
-function pauseGame() {
-  console.log(`Game paused`);
-  // timerPause();
-  show_hide(gameOverScreen, gameScreen);
-}
+// function pauseGame() {
+//   console.log(`Game paused`);
+//   // timerPause();
+//   show_hide(gameOverScreen, gameScreen);
+// }
 
 function mainMenu() {
   console.log(`Returning to main menu`);
@@ -70,4 +72,73 @@ function playAgain() {
   show_hide(gameScreen, gameOverScreen);
   startGame();
   // reset score and the timer
+}
+
+// implement game
+const holes = document.querySelectorAll(".hole");
+const scoreBoard = document.getElementById("current-score");
+const moles = document.querySelectorAll(".mole");
+
+let lastHole;
+let timeUp = false;
+let score = 0;
+
+// console.log(`holes : ${holes} scoreBoardd : ${scoreBoard} moles : ${moles}`)
+
+function randomTime(min, max) {
+  return Math.round(Math.random() * (min - max) + max);
+}
+
+// console.log(randomTime(0,100));
+
+function randomHole(holes) {
+  const ids = Math.floor(Math.random() * holes.length);
+  const hole = holes[ids];
+
+  if (hole === lastHole) {
+    console.log("same last hole");
+    return randomHole(holes);
+  }
+
+  lastHole = hole;
+  return hole;
+}
+
+// console.log(randomHole(holes));
+
+function peep() {
+  const time = randomTime(200, 1000);
+  const hole = randomHole(holes);
+
+  // console.log(`time : ${time}, hole : ${hole}`);
+
+  hole.classList.add("up");
+
+  setTimeout(() => {
+    hole.classList.remove("up");
+    if (!timeUp) peep();
+  }, time);
+}
+
+
+function whack(e){
+  score++;
+  this.parentNode.classList.remove("up");
+  // scoreBoard.textContent = score;
+  scoreBoard.innerHTML = score;
+}
+
+moles.forEach((mole) => mole.addEventListener("click", whack));
+
+
+function startWhack() {
+  // scoreBoard.textContent = 0;
+  scoreBoard.innerHTML = 0;
+  timeUp = false;
+  score = 0;
+  peep();
+  setTimeout(() => {
+    timeUp = true;
+    console.log("game over");
+  }, GAME_TIME_SECONDS * 1000);
 }
